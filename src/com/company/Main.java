3266d178f2f7e2b9ad75903cd219e4d71a0e913c
sparrow84 <vorobey280184@mail.ru>
2019-02-17 14:15:25 +0700

    Init

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,12 +6,6 @@ import com.company.animals.Flying;
 public class Main {
 
     public static void main(String[] args) {
-        Init init = new Init();
 
-        Animal fl = new Flying();
-
-        int[] arr = fl.move();
-
-        System.out.println(arr[0] + "  " + arr[1]);
     }
 }
